fbcb5d55fe7d6c34bddc7e2615f6b88f7bcbfa71
frank@alberovalley <alberovalley@gmail.com>
2013-08-29 16:31:17 +0200

    cambiada preferencias para evitar petes en versiones antiguas; corregido tamaño mínimo

--- gen/com/alberovalley/novedadesumbria/R.java
+++ gen/com/alberovalley/novedadesumbria/R.java
@@ -25,14 +25,13 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090007;
-        public static final int btCompruebaNovedades=0x7f090004;
-        public static final int btNavegaNovedades=0x7f090006;
+        public static final int action_settings=0x7f090006;
+        public static final int btCompruebaNovedades=0x7f090003;
+        public static final int btNavegaNovedades=0x7f090005;
         public static final int button1=0x7f090001;
-        public static final int configfrag=0x7f090002;
-        public static final int layout_widget=0x7f090003;
+        public static final int layout_widget=0x7f090002;
         public static final int textview1=0x7f090000;
-        public static final int tvRespuestaUmbria=0x7f090005;
+        public static final int tvRespuestaUmbria=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/alberovalley/novedadesumbria/SettingsActivity.java
+++ src/com/alberovalley/novedadesumbria/SettingsActivity.java
@@ -1,14 +1,14 @@
 package com.alberovalley.novedadesumbria;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-
+        addPreferencesFromResource(R.xml.preferences);
     }
 
 }
